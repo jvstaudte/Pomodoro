a54450cfe2f71ba2237a05df2aec9cca3aec9910
James Staudte <jvstaudte@yahoo.com>
2024-06-29 15:07:26 -0700

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by James Staudte on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
